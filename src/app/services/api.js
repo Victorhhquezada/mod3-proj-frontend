3f25799bba368e1cbe2dc9deecbacea636a070aa
victorhhq <victorhhquezada@gmail.com>
2021-09-16 19:43:48 -0500

    agregar el enlace

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //indica si está deployada o no
 const isProduction = process.env.NODE_ENV === "production";
 
-const baseURL = isProduction ? "www.paginadploy.com/api" : "http://localhost:3001/api";
+const baseURL = isProduction ? "https://brgr-club.herokuapp.com/api" : "http://localhost:3001/api";
 
 axios.defaults.withCredentials = true
 
